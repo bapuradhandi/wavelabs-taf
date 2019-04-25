145a1e928fe4caf59245483177e6ac6e4df53fe4
bapu <bapu@nblap245.nbos.vinisoft.com>
2019-04-25 15:37:07 +0530

    ym change

---
+++ src/test/java/com/pwc/tests/steps/ApiSteps.java
@@ -0,0 +1,32 @@
+package com.pwc.tests.steps;
+
+
+import com.pwc.tests.pages.LandingPage;
+import com.taf.test.framework.helpers.UrlBuilder;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+
+public class ApiSteps {
+
+
+    @When("^user retrieves a specific api information$")
+    public void getApiInformation() throws Throwable {
+    	
+
+        
+    }
+
+//    @And("^user enters password \"([^\"]*)\"$")
+//    public void userEntersPassword(String password) throws Throwable {
+//        landingPage.enterPassword(password);
+//    }
+//
+//    @When("^user clicks on sign in button$")
+//    public void userClicksOnSignInButton() throws Throwable {
+//        landingPage.clickOnSignInBtn();
+//    }
+    
+
+}
